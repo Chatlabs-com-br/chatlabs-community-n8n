c972d9ccd01666ab88f3add706593a1d64b7d04d
Eduardo Ribeiro <eduardoxrib@gmail.com>
2026-04-06 13:54:57 -0300

    Revert "refactor: improve method structures"
    This reverts commit d304f2f4e84f86f00654fa2073d47bba4e577990.

--- dist/resources/chat/transferChat.d.ts
+++
@@ -1,4 +0,0 @@
-import { IExecuteFunctions, INodeProperties, IDataObject } from "n8n-workflow";
-export declare const fields: INodeProperties[];
-export declare function execute(context: IExecuteFunctions, i: number, baseUrl: string, _headers: Record<string, string>, headersJson: Record<string, string>): Promise<IDataObject>;
-//# sourceMappingURL=transferChat.d.ts.map
